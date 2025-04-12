b2ae3eeb8dfd82a27e8d23d1461ab3e89b3825f7
itssaimaKhan <samiakhan8754@gmail.com>
2025-04-12 23:58:18 +0500

    main

--- data-cleaner/main.py
+++ data-cleaner/main.py
@@ -2,3 +2,15 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 
+st.set_page_config(page_title=" File Converter & Cleaner", layout="wide")
+st.title("File Converter & Cleaner")
+st.write("Upload Your CSV and Excel Files to clean the data convert formats effortlessly")
+
+files = st.file_uploader("Upload CSV or Excel files", type=["csv", "xlsx"], accept_multiple_files=True)
+
+if files:
+    for file in files:
+        ext = file.name.split(".")[-1]
+        
+        data.pdf.csv
+
