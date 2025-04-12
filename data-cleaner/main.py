7a958a828b0a85fba117e582d9af0700ed5ca52b
itssaimaKhan <samiakhan8754@gmail.com>
2025-04-13 01:27:15 +0500

    main

--- data-cleaner/main.py
+++ data-cleaner/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from io import BytesIO
 
 st.set_page_config(page_title=" File Converter & Cleaner", layout="wide")
-st.title("File Converter & Cleaner")
+st.title("📁File Converter & Cleaner")
 st.write("Upload Your CSV and Excel Files to clean the data convert formats effortlessly")
 
 files = st.file_uploader("Upload CSV or Excel files", type=["csv", "xlsx"], accept_multiple_files=True)
@@ -11,6 +11,38 @@ files = st.file_uploader("Upload CSV or Excel files", type=["csv", "xlsx"], acce
 if files:
     for file in files:
         ext = file.name.split(".")[-1]
+        df = pd.read_csv(file) if ext == "csv" else pd.read_excel(file)
         
-        data.pdf.csv
+        st.subheader(f"🔍{file.name}I- Preview")
+        st.dataframe(df.head())
+        
+        if st.checkbox(f"Fill Missing Values- {file.name}"):
+            df.fillna(df.select_dtypes(include=["number"]).mean(), inplace=True)
+            st.success("Missing values fill successfully!")
+            st.dataframe(df.head())
+            
+            selected_columns = st.multiselect("Select Columns - {file.name}", df.columns, default=df.columns)
+            df = df[selected_columns]
+            st.dataframe(df.head())
+            
+            if st.checkbox(f"📊 Show Chart - {file.name}") and not df.select_dtypes(include="number").empty:
+                st.bar_chart(df.select_dtypes(include="number").iloc[:, :2])
+                
+                format_choce = st.radio(f"Convert {file.name} to:", ["CSV", "Excel"], key=file.name)
+                
+                if st.button(f"⬇️ Download {file.name} as {format_choce}"):
+                    output = BytesIO()
+                    if format_choce == "CSV":
+                        df.to_csv(output, index=False)
+                        mime = "text/csv"
+                        new_name = file.name.replace(ext, "csv")
+                    else:
+                        df.to_excel(output, index=False)
+                        mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                        new_name = file.name.replace(ext, "xlsx")
+                        output.seek(0)
+                    st.download_button("⬇️ Download File", file_name=new_name, data=output, mime=mime)
+                    st.success("Processing Complete! 🎉")
+        
+    
 
